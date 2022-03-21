83060d3ac0f7ba5dd265837919499f25e93a7559
K-Bowler <Nebulasends@gmail.com>
2022-03-21 19:06:40 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+var width = 10;
+var height = 10;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 20;
+var payment = 25;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', change);
+
+var quizzes = 84;
+var midterm = 86;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Kevin';
+var lastName = 'Bowler';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 4;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 24;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
