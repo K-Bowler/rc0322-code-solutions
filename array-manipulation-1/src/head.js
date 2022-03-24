c4850c92eb08167ba74ae5631c9c3bd2a2b779b5
K-Bowler <Nebulasends@gmail.com>
2022-03-24 23:43:39 +0000

    Completed array-manipulation-1

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,7 @@
+/* exported head */
+function head(array) {
+  return array[0];
+}
+
+// define a new function called head with one parameter (array)
+// within the function return the first element in the array
