2026a78562185d5dc567a89ff9ab4a37cb401ae0
K-Bowler <Nebulasends@gmail.com>
2022-04-29 00:51:21 +0000

    javascript-typing-tutor WIP#1

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,10 @@
+var $typingText = document.querySelectorAll('span');
+var letters =
+
+document.addEventListener('keydown', function (event) {
+  for (var i = 0; i < $typingText.length; i++) {
+    if (event.target === $typingText[i].classList = 'active') {
+      if ()
+    }
+  }
+});
