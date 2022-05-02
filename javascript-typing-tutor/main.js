012e6d57134d1d003e58fc12190cec72f30e71c6
K-Bowler <Nebulasends@gmail.com>
2022-05-02 17:01:03 +0000

    javascript-typing-tutor WIP#2

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,12 @@
 var $typingText = document.querySelectorAll('span');
-var letters =
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 document.addEventListener('keydown', function (event) {
   for (var i = 0; i < $typingText.length; i++) {
-    if (event.target === $typingText[i].classList = 'active') {
-      if ()
+    for (var l = 0; l < letters.length; l++) {
+      if (event.key === $typingText[i].classList = 'active') {
+      }
     }
   }
 });
