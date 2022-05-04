d48aa8ca69742c091a45b8c2eca443ba10c7a1a3
K-Bowler <Nebulasends@gmail.com>
2022-05-04 18:42:15 +0000

    removed console logs

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -18,27 +18,19 @@ document.addEventListener('keydown', function (event) {
     $typingText[currentIndex].classList.add('correct');
     currentIndex++;
     totalCorrect++;
-    // console.log('CONDITION:', currentIndex < $typingText.length);
     if (currentIndex < $typingText.length) {
       $typingText[currentIndex].classList.add('active');
     } else {
-    // console.log('Reached the END!');
       $resetBtn.classList.remove('hidden');
     }
   } else {
     $typingText[currentIndex].classList.add('incorrect');
   }
   $accuracy.textContent = (totalCorrect / totalPresses).toFixed(2) * 100 + '%';
-
-  // console.log('EVENT:', typedLetter);
-  // console.log('ELEMENT:', ele);
-  // console.log('MATCH:', ele.textContent === typedLetter);
 });
 
 $resetBtn.addEventListener('click', function (event) {
-  // console.log('hi');
   for (var i = 0; i < $typingText.length; i++) {
-  // console.log('index:', i);
     $typingText[i].classList.remove('incorrect', 'correct');
   }
   currentIndex = 0;
