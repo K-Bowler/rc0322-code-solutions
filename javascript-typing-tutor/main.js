971edc9e74d5b181763722ed78986c4060bafab3
K-Bowler <Nebulasends@gmail.com>
2022-05-04 17:23:51 +0000

    javascript-typing-tutor WIP#2

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,12 +1,50 @@
-var $typingText = document.querySelectorAll('span');
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const $typingText = document.querySelectorAll('span');
+const $resetBtn = document.querySelector('.reset-btn');
+const $accuracy = document.querySelector('#accuracy');
+let currentIndex = 0;
+let totalPresses = 0;
+let totalCorrect = 0;
 
 document.addEventListener('keydown', function (event) {
-  for (var i = 0; i < $typingText.length; i++) {
-    for (var l = 0; l < letters.length; l++) {
-      if (event.key === $typingText[i].classList = 'active') {
-      }
-    }
+  if (currentIndex === $typingText.length) {
+    return;
   }
+  totalPresses++;
+  const ele = $typingText[currentIndex];
+  const typedLetter = event.key;
+
+  if (ele.textContent === typedLetter) {
+    $typingText[currentIndex].classList.remove('incorrect', 'active');
+    $typingText[currentIndex].classList.add('correct');
+    currentIndex++;
+    totalCorrect++;
+    // console.log('CONDITION:', currentIndex < $typingText.length);
+    if (currentIndex < $typingText.length) {
+      $typingText[currentIndex].classList.add('active');
+    } else {
+    // console.log('Reached the END!');
+      $resetBtn.classList.remove('hidden');
+    }
+  } else {
+    $typingText[currentIndex].classList.add('incorrect');
+  }
+  $accuracy.textContent = (totalCorrect / totalPresses).toFixed(2) * 100 + '%';
+
+  // console.log('EVENT:', typedLetter);
+  // console.log('ELEMENT:', ele);
+  // console.log('MATCH:', ele.textContent === typedLetter);
+});
+
+$resetBtn.addEventListener('click', function (event) {
+  // console.log('hi');
+  for (var i = 0; i < $typingText.length; i++) {
+  // console.log('index:', i);
+    $typingText[i].classList.remove('incorrect', 'correct');
+  }
+  currentIndex = 0;
+  totalPresses = 0;
+  totalCorrect = 0;
+  $accuracy.textContent = '0%';
+  $resetBtn.classList.add('hidden');
+  $typingText[currentIndex].classList.add('active');
 });
