056b2b30b9b47e5fcc0d88f5773ca29b45bd2d70
K-Bowler <Nebulasends@gmail.com>
2022-05-16 02:41:52 +0000

    Completed sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -160,7 +160,36 @@ app.put('/api/grades/:gradeId', (req, res) => {
 });
 
 app.delete('/api/grades/:gradeId', (req, res) => {
-
+  const gradeId = Number(req.params.gradeId);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: '"gradeId" must be a positive integer'
+    });
+    return;
+  }
+  const sql = `
+    delete from "grades"
+    where "gradeId" = $1
+    returning *;
+  `;
+  const params = [gradeId];
+  db.query(sql, params)
+    .then(result => {
+      const dltGrade = result.rows[0];
+      if (!dltGrade) {
+        res.status(404).json({
+          error: `Cannot find grade with "gradeId" ${gradeId}`
+        });
+      } else {
+        res.json(dltGrade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
 });
 
 app.listen(3000, () => {
