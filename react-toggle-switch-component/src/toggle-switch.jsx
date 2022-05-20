17653430467f939eb86b84058fdca0298424a7eb
K-Bowler <Nebulasends@gmail.com>
2022-05-20 19:34:34 +0000

    react-toggle-switch-component start

---
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// class toggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.clicked = this.clicked.bind(this);
+//     this.state = { totalClicks: 0 };
+//   }
+
+//   clicked() {
+//     this.setState({ totalClicks: this.state.totalClicks + 1 });
+//   }
+
+//   render() {
+//     const totalClicks = this.state.totalClicks;
+//     const btnWarmth = 'freeze';
+//     return <button className={`btn ${btnWarmth}`} onClick={this.clicked}>Hot Button</button>;
+//   }
+// }
+
+// export default toggleSwitch;
